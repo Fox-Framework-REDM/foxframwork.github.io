75dd611e7a98b970cc8f28236b83ca5e908d28a1
Erick Henrique <erickhenriquediass@gmail.com>
2026-06-18 21:29:26 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider/next';
+import { Provider } from '@/components/provider';
 import type { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
